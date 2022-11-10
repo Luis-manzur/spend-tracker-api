6a975f8cd044b0befcb0e3830ba3f9fde0929ef4
luism <luismanzur91@outlook.com>
2022-11-09 22:00:32 -0400

    add goals urls

--- transactions/urls.py
+++ transactions/urls.py
@@ -3,11 +3,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views import goals as goals_views
 from .views import transactions as transaction_views
 
 router = DefaultRouter()
 router.register(
     r"transactions", transaction_views.TransactionViewSet, basename="transactions"
+)
+router.register(
+    r"goals", goals_views.GoalsViewSet, basename="goals"
 )
 
 urlpatterns = [path("", include(router.urls))]
